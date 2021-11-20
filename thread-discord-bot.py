9b311242e04d508e4357289c2b21c2bcca170847
Hayley Davies <cd230099@falmouth.ac.uk>
2021-11-20 19:08:57 +0000

    Created basic bot functionality
    - Bot should create a thread at 9am every morning and ping the correct group of people

---
+++ thread-discord-bot.py
@@ -0,0 +1,33 @@
+import datetime, discord, aiocron
+
+client = discord.Client()
+
+token = open('token.txt', 'r').read()
+template = open('template.txt', 'r').read()
+
+channel_id = 868379994710224938
+role_id = 885847934619512833
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    create_daily_thread.start()
+
+@aiocron.crontab('* * * * *')
+async def create_daily_thread():
+    thread_parent = client.get_channel(channel_id)
+    thread_name = generate_thread_name()
+
+    print(f'Got channel {thread_parent}')
+
+    thread_start_message = await thread_parent.send(template)
+    thread = await thread_parent.create_thread(name=thread_name, message=thread_start_message)
+    await thread.send(f'<@&{role_id}>')
+
+def generate_thread_name():
+    date = datetime.date.today()
+    dateformatted = str(date).replace('-', '.')
+    name = '{0} Update'.format(dateformatted)
+    return name
+
+client.run(token)
