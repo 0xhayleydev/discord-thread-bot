837290aaa45a1669f9c870120a3615d0c1df9eb2
Hayley Davies <cd230099@falmouth.ac.uk>
2022-10-28 17:25:15 +0100

    feature: added ability for bot to work in multiple servers

--- thread-discord-bot.py
+++ thread-discord-bot.py
@@ -1,60 +1,127 @@
-"""required imports are
-datetime - to get today's date, for the thread name
-discord - to run the bot
-aiocron - to run tasks at set times"""
-import datetime, discord, aiocron
+from http import server
+from xmlrpc.client import Server
+import discord
+from discord import app_commands
+import datetime
+import json
+import aiocron
 
-"""create the client"""
-client = discord.Client()
+class ServerInfo:
+    def __init__(self, save_data: bool, guild, channel = "", role = "", last_thread = ""):
+        self.guild = str(guild)
+        if save_data:
+            self.channel = str(channel)
+            self.role = str(role)
+            self.last_thread = str(last_thread)
+            self.saveToJSON()
+            self.exists = True
+        else:
+            self.loadFromJSON()
+        
+    def __str__(self):
+        output = ''
+        output = output + f'guild: {self.guild}, '
+        output = output + f'channel: {self.channel}, '
+        output = output + f'role: {self.role}, '
+        output = output + f'last_thread: {self.last_thread}, '
+        return output
+        
+    def saveToJSON(self):
+        data = json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+        f = open(f'guilds/{self.guild}.guild', 'w')
+        f.write(data)
+        f.close()
 
-"""Read from a file the bot's token and the template message the bot will send"""
-token = open('token.txt', 'r').read()
-template = open('template.txt', 'r').read()
-
-"""the role id to ping & channel id to create the thread in"""
-channel_id = 898154481894694932
-role_id = 885847934619512833
-
-"""when the bot starts
-1. output that the bot has connected
-2. start the async event"""
-@client.event
-async def on_ready():
-    print(f'I am {client.user}')
-    create_daily_thread.start()#
-
-"""create a daily thread at 9am on weekdays (using cron)"""
-@aiocron.crontab('0 9 * * *')
-async def create_daily_thread():
-    f = open('previous_id.txt', 'r')
-    previous_id = f.read()
-    f.close()
-
-    if (previous_id != ""):
-        previous_id = int(previous_id)
-        previous_thread = client.get_channel(previous_id)
-        await previous_thread.edit(archived = True)
-
-    date = datetime.date.today()
-    if (date.weekday() in [5, 6]):
-        return
+    def loadFromJSON(self):
+        try:
+            raw = self.load_file()
+            data = json.loads(raw)
+            self.guild = data["guild"]
+            self.channel = data["channel"]
+            self.role = data["role"]
+            self.last_thread = data["last_thread"]
+            self.exists = True
+        except FileNotFoundError:
+            self.exists = False
     
-    thread_parent = client.get_channel(channel_id)
-    thread_name = generate_thread_name(date)
+    def load_file(self):
+        f = open(f'guilds/{self.guild}.guild', 'r')
+        data = f.read()
+        f.close()
+        return data
+        
+class bot_client(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False
+    
+    async def on_ready(self):
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f"I am {self.user}.")
+        aiocron.crontab("0 0 * * 1-5", func=create_daily_threads, start=True)
+        
+client = bot_client()
+tree = app_commands.CommandTree(client)
 
-    print(f'Got channel {thread_parent}')
+async def create_daily_threads():
+    for i in range(len(client.guilds)):
+        server_info = ServerInfo(False, client.guilds[i].id)
+        if (server_info.exists == True):
+            thread = await create_thread(server_info.guild, server_info.channel, server_info.role)
+            print(server_info.last_thread)
+            await close_thread(server_info.guild, server_info.last_thread)
+            server_info.last_thread = str(thread.id)
+            server_info.saveToJSON()
 
-    thread_start_message = await thread_parent.send(template)
-    thread = await thread_parent.create_thread(name=thread_name, message=thread_start_message)
-    await thread.send(f'<@&{role_id}>')
-    f = open('previous_id.txt', 'w')
-    f.write(str(thread.id))
-    f.close()
+def get_discord_channel(guild_id, channel_id):
+    channel = client.get_guild(int(guild_id)).get_channel_or_thread(int(channel_id))
+    return channel
 
-"""create a thread name from the date"""
-def generate_thread_name(date):
-    name = f'{date.day}.{date.month}.{date.year} Update'
-    return name
+async def close_thread(guild_id, channel_id):
+    thread = get_discord_channel(guild_id, channel_id)
+    if (thread != None):
+        await thread.edit(archived=True)
+        
+async def create_thread(guild_id, channel_id, role_id):
+    thread_name = get_thread_name()
+    channel = get_discord_channel(guild_id, channel_id)
+    thread_with_message = await channel.create_thread(name=thread_name, content=scrum_message)
+    thread = thread_with_message.thread
+    await thread.send(f"<@&{role_id}>\n\nPLEASE NOTE: Bot is in BETA. Please contact hayley#1811 (<@175635927954227200>) if you have any issues or suggestions.")
+    return thread
 
-"""start the bot"""
+def get_thread_name():
+    date = datetime.date.today()
+    thread_name = f'[{date.day}.{date.month}.{date.year}] Update'
+    return thread_name
+
+async def create_template_post(interaction, channel, role):
+    await interaction.response.send_message(f"Working...", ephemeral = True)
+    thread = await create_thread(interaction.guild.id, channel.id, role.id)
+    await interaction.edit_original_response(content=f"I will ping <@&{role.id}> in <#{channel.id}>")
+    return thread
+
+# COMMANDS
+@tree.command(name = "scrum", description = "Setup the scrum message in the specified channel.")
+async def self(interaction: discord.Interaction, channel: discord.ForumChannel, role: discord.Role):
+    thread = await create_template_post(interaction, channel, role)
+    server_info = ServerInfo(True, interaction.guild.id, channel.id, role.id, thread.id)
+    server_info.loadFromJSON()
+
+@tree.command(name = "test", description = "Test the bot.", guild = discord.Object(id = 713118191697788974))
+async def self(interaction: discord.Interaction):
+    server_info = ServerInfo(False, interaction.guild.id)
+    await create_daily_threads()
+    await interaction.response.send_message(f"Request Sent", ephemeral = True)
+    server_info.loadFromJSON()
+    
+@tree.command(name = "get_data", description = "Print the JSON data that is stored on the server.")
+async def self(interaction: discord.Interaction):
+    server_info = ServerInfo(False, interaction.guild.id)
+    await interaction.response.send_message(server_info.load_file(), ephemeral=True)
+
+scrum_message = open('template.txt', 'r').read()
+token = open('token.txt', 'r').read()
 client.run(token)
